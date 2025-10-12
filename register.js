7a24b6961697a5b78be856a3cb72f13c49c895cc
John Carl Ola <120295019+oppapi@users.noreply.github.com>
2025-10-13 00:24:46 +0800

    Update register.js

--- register.js
+++ register.js
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
           localStorage.setItem('userCleanzorID', cleanzorID);
         }
 
-        window.showLoadingAndRedirect('home.html'); 
+        window.showLoadingAndRedirect('index.html'); 
       })
       .catch((error) => {
         console.error('Signup error:', error);
