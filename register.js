437a2e1a04052c693d7abb147167887c5517ede8
John Carl Ola <120295019+oppapi@users.noreply.github.com>
2025-10-13 10:54:39 +0800

    Update register.js

--- register.js
+++ register.js
@@ -132,8 +132,8 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
 
-  const signupBtn = document.getElementById('signupBtn');
-  if (signupBtn) {
+  const loginBtn = document.getElementById('loginBtn');
+  if (loginBtn) {
     loginBtn.addEventListener('click', function(e) {
       e.preventDefault();
       window.showLoadingAndRedirect('index.html');
